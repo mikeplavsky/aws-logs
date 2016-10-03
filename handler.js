1020d2b974e84671c62873bd026c4127b16ff5e3
mikeplavsky <mike.plavsky@quest.com>
2016-10-03 14:20:37 +0000

    getting log streams

--- handler.js
+++ handler.js
@@ -1,7 +1,33 @@
 'use strict';
 
 let AWS = require("aws-sdk");
+
 let l = new AWS.Lambda();
+let logs = new AWS.CloudWatchLogs();
+
+module.exports.get_streams = (group) => {
+
+    logs.describeLogStreams({
+
+        logGroupName: group,
+        descending: true,
+        orderBy: 'LastEventTime'
+
+    },(err, data)=>{
+
+        if (err != null){
+            console.log(err);
+            return 
+        }
+
+        data.logStreams.forEach((v) => {
+            console.log(new Date(v.lastEventTimestamp));
+            console.log(v.logStreamName);
+        });
+
+    });
+
+}
 
 module.exports.groups = (event, context, cb) => {
     
