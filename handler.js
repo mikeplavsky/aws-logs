ed91adf1c2806fa6dad0584ea60df7e735bed079
mikeplavsky <mike.plavsky@quest.com>
2016-10-03 15:39:04 +0000

    added slack

--- handler.js
+++ handler.js
@@ -5,7 +5,9 @@ let AWS = require("aws-sdk");
 let l = new AWS.Lambda();
 let logs = new AWS.CloudWatchLogs();
 
-module.exports.get_streams = (group) => {
+let config = require("./config.js").config;
+
+let get_streams = (group) => {
     
     let now = new Date();
     now.setHours(now.getHours() - 1);
@@ -36,30 +38,33 @@ module.exports.get_streams = (group) => {
 }
 
 module.exports.groups = (event, context, cb) => {
+
+    config.logGroups.forEach(v=>{
     
-    //var cfg = require('dotenv').config();
+        l.invoke({
 
-    l.invoke({
+            FunctionName: "aws-logs-prod-streams",
+            InvocationType:'Event',
+            Payload: `{"group":"${v}"}`
 
-        FunctionName: "aws-logs-prod-streams",
-        InvocationType:'Event',
-        Payload: `{"group":"test 1"}`
+        },(err,data) => {
 
-    },(err,data) => {
+            console.log(err);
+            console.log(data);
 
-        console.log(err);
-        console.log(data);
+        });
 
     });
-    
+
     console.log("called it.")
 
 };
 
 module.exports.streams = (event, context, cb) => {
 
-    console.log("got it.")
-    console.log(event);
-    console.log("done");
+    console.log(`working on ${event.group}`)
+    get_streams(event.group);
+
+    console.log(`done ${event.group}`);
 
 }
